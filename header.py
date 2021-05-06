e458b6e2b55f43a99d223e6e3c08d60d9035f8bb
Leonardo Villalobos <levillalobos@csumb.edu>
2021-05-05 18:45:45 -0700

    add headers

--- header.py
+++ header.py
@@ -1,7 +1,10 @@
 import sys
+import PySide6.QtGui
+import urllib.request
 from PySide6.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton,
     QLineEdit, QHBoxLayout, QVBoxLayout
 )
 from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QImage
 from PySide6.QtCore import (Slot, Qt)
