2e76b6fb2efec650152a9fd9be3d051d42fad858
Leonardo Villalobos <levillalobos@csumb.edu>
2021-05-09 12:56:19 -0700

    fix documentation

--- trim.py
+++ trim.py
@@ -2,16 +2,19 @@ def trim_string(string: str, length: int) -> str:
     """
     Ensure that the length of a string does not exceed `length` characters.
 
-    If length of `string` is greater than `length`, a shortened copy of the 
-    string is returned with elipses in place of the first three removed
-    characters. Otherwise the original string is returned.
+    > Preconditions:`length` > 0
 
-    `string` : the string which is passed in for trimming.
-    `length` : the size which the string will be trimmed to.
+    > Postconditions: If length of `string` is greater than `length`, a
+    shortened copy of the string is returned with elipses in place of 
+    the first three removed characters. Otherwise the original string is returned.
+
+    + `string` : the string which is passed in for trimming.
+    + `length` : the size which the string will be trimmed to.
     """
     # Sample runs:
-    # trim_window_title("Baby shark") -> "Baby shark"
-    # trim_window_title("ABCDEFGHIJKLMNOPQRSTUVWXYZ_ABCDEFGHIJKLMNOPQRSTUVWXYZ") -> "ABCDEFGHIJKLMNOPQRSTUVWXYZ_ABCDEFGHIJKLMNOPQR..."
+    # trim_string("Baby shark", 10) -> "Baby shark"
+    # trim_string("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5) -> "ABCDE..."
+    # trim_string("ABC", -1) -> Error! length must be greater than -1.
 
     pass  # TODO: Implementation goes here. `pass` keyword is used to tell
     # python that this function is not yet written.
