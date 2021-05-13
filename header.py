d839e936dcc6a57acf488f60b1f451256f7375e0
Leonardo Villalobos <levillalobos@csumb.edu>
2021-05-13 13:22:42 -0700

    add imports from base-code branch

--- header.py
+++ header.py
@@ -10,4 +10,17 @@ from PySide6.QtGui import QImage
 from PySide6.QtCore import (Slot, Qt)
 from PySide6.QtWidgets import QListWidget
 
+# NOTE: New imports from base-code branch:
+import subprocess
+from googleapiclient.discovery import build
+import time
+import vlc
+import pafy
+from PIL import Image
+from PIL.ImageQt import ImageQt
+from requests import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+
 author_names = "Leonardo Villalobos, Luz Violeta Robles, Austin Seidel"
